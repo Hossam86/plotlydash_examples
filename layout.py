383124a2b3adabdb67ba46cc907d65955f57b605
Hossam Samir <hossamsamir@outlook.com>
2022-05-25 03:24:51 +0200

    reformat

--- layout.py
+++ layout.py
@@ -1,4 +1,3 @@
-
 from dash import Dash, html, dcc
 #  interface for plotly express
 import plotly.express as px
@@ -18,41 +17,35 @@ import pandas as pd
 #  Application instance from Dash class
 app = Dash(__name__)
 
-#  sample data source - pandas dataframe
-df = pd.DataFrame(
-    {"Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-     "Amount": [4, 1, 2, 2, 4, 5],
-     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]})
+#  sample datasource -- panndas frame
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-#  dash built on react.js and ploty.js , so we can use plolty graph directly through dash core interface
+# plotly express figure -- you can use any plotly figure check plotly.js docs
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-style = {'background': colors['background'],
-         'textAlign': 'center',
-         'color': colors['text']}
 
-barplot_graph_c = dcc.Graph(id='exaple_barplot', figure=fig)
+# simple layout as list of html components(H1,p, ) and dash core components(plotly graph)
+layout1 = [html.H1(children='Hello Dash'),
+           html.Div(children='''Dash: A web application framework for your data.'''),
+           dcc.Graph(id='example-graph1', figure=fig)]
 
-barplot_title_c = html.H1(children="Hello Dash!", style=style)
-barplot_subtitle_c = html.Div(
-    children="Dash: A web application framework for your data.")
+# Let's customize the text in our app by modifying the inline styles of the components.
+# The style property in HTML is a semicolon-separated string. In Dash, you can just supply a dictionary.
+# The keys in the style dictionary are camelCased. So, instead of text-align, it's textAlign.
+# The HTML class attribute is className in Dash.
 
-app.layout = html.Div(
-    children=[barplot_title_c, barplot_subtitle_c, barplot_graph_c], style=style)
+colors = {'background': '#111111', 'text': '#7FDBFF'}
 
+layout2 = [html.H1(children='Hello Dash', style={'textAlign': 'center', 'color': colors['text']}),
+           html.Div(children='''Dash: A web application framework for your data.''',
+                    style={'textAlign': 'center', 'color': colors['text']}),
+           dcc.Graph(id='example-graph2', figure=fig)]
 
-def main():
-    app.run_server(debug=True)
+layout = layout1 + layout2
 
-
+app.layout = html.Div(children=layout)
 if __name__ == '__main__':
-    main()
+    app.run_server(debug=True)
